aa6f598de174410a7cdc7df50a2f19983365e1c6
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-11-12 17:39:05 +0200

    Added custom 'Figtree' font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Adjust paths if necessary
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        figtree: ['Figtree', 'Arial', 'sans-serif'], // 'custom' is the name you'll use in your classes
+      },
+    },
   },
   plugins: [],
-}
+};
