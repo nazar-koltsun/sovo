02b002ec98a625dcb68019f7b8283a5d7a93c326
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-11-12 15:40:30 +0200

    Basic App.jstemplate structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const App = () => {
+  return (
+    <div className="grid grid-cols-[260px_1fr] grid-rows-[auto_1fr] h-screen">
+      <header className="col-start-2">header</header>
+      <aside className="col-start-1 row-start-1 row-end-[-1]">sidebar</aside>
+      <main className="col-start-2">main content</main>
+    </div>
+  );
 }
 
 export default App;
