10f97a21b0e2902883844158f8212c8f34763264
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-11-13 23:39:10 +0200

    Minor layout fix

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 const RootLayout = () => {
   return (
     <>
-      <div className="grid grid-cols-[260px_1fr] grid-rows-[auto_1fr] pr-2.5 bg-[#EDEDF2] font-figtree text-sm">
+      <div className="grid grid-cols-[260px_1fr] grid-rows-[auto_1fr] min-h-screen pr-2.5 bg-[#EDEDF2] font-figtree text-sm">
         <Header />
         <Sidebar />
         <main className="col-start-2 bg-[#F9F9FC] border-t-[2px] border-t-[#EEF2F7]">
