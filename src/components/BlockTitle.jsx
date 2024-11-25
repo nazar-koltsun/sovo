7a55029d115dffbd705db734fafb47763577a798
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-11-25 19:29:42 +0200

    BlockTitle cmp modification

--- src/components/BlockTitle.jsx
+++ src/components/BlockTitle.jsx
@@ -1,6 +1,6 @@
-const BlockTitle = ({ children }) => {
+const BlockTitle = ({className, children }) => {
   return (
-    <h2 className="text-base font-semibold text-[var(--electric-blue)]">{children}</h2>
+    <h2 className={`${className} text-base font-semibold text-[var(--electric-blue)]`}>{children}</h2>
   );
 };
 
